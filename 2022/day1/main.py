186f5e111678e801813f065b70794d1e0609f3e4
PaperBackPear3 <fabioangeloni2001@gmail.com>
2023-11-07 22:07:17 +0100

    day one

---
+++ 2022/day1/main.py
@@ -0,0 +1,39 @@
+from os import getcwd
+
+
+file1 = open(getcwd() + "/2022/day1/input.txt", "r")
+Lines = file1.readlines()
+
+max_val = 0
+
+bigger_max = 0
+medium_max = 0
+smaller_max = 0
+
+for line in Lines:
+    if line != "\n":
+        max_val += int(line)
+    if line == "\n":
+        if max_val > bigger_max:
+            smaller_max = medium_max
+            medium_max = bigger_max
+            bigger_max = max_val
+        elif max_val > medium_max:
+            smaller_max = medium_max
+            medium_max = max_val
+        elif max_val > smaller_max:
+            smaller_max = max_val
+        max_val = 0
+file1.close()
+
+print(
+    "res: "
+    + str(
+        [
+            bigger_max,
+            medium_max,
+            smaller_max,
+            sum([bigger_max, medium_max, smaller_max]),
+        ]
+    )
+)
