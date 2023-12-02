fe7f2fbbe946a03fc727955093f4fdae4f3b6331
PaperBackPear3 <fabioangeloni2001@gmail.com>
2023-12-02 15:42:52 +0100

    added day 3 starting

---
+++ 2023/day3/main.py
@@ -0,0 +1,9 @@
+from sys import argv
+import os
+
+
+file_name = argv[1]
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+file1 = open(dir_path + "/" + file_name, "r")
+Lines = file1.read().splitlines(keepends=False)
