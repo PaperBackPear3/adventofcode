f185a2746c6058cf346c0b61247db60d12e7883f
PaperBackPear3 <fabioangeloni2001@gmail.com>
2023-12-05 20:02:42 +0100

    finished day 5

--- 2023/day5/main.py
+++ 2023/day5/main.py
@@ -9,24 +9,52 @@ file1 = open(dir_path + "/" + file_name, "r")
 read_lines = file1.read().splitlines(keepends=False)
 
 total_points = 0
-#seeds = int(read_lines[0].split(" "))
+# seeds = int(read_lines[0].split(" "))
+
 
 def get_steps_maps_from_lines(lines):
-    steps = []
+    steps = {}
     index = 0
+    prev_line_key = ""
     for line in lines:
-        if line is not "":
+        if line != "":
             if index == 0:
-                steps.append([line])
+                steps[line] = []
+                prev_line_key = line
             if index > 0:
-                steps[-1].append([int(character) for character in line.split() if character.isdigit()])
+                steps[prev_line_key].append(
+                    [
+                        int(character)
+                        for character in line.split()
+                        if character.isdigit()
+                    ]
+                )
+
             index += 1
         else:
             index = 0
     return steps
 
-steps = get_steps_maps_from_lines(read_lines)
-seeds = [int(character) for character in steps[0][0].split(" ") if character.isdigit()]
 
+def get_map_for_next_step(step, seed):
+    next_map = seed
+    for index, value in enumerate(step):
+        if seed >= value[1] and seed < (value[1] + value[2]):
+            next_map = seed + (value[0] - value[1])
+            #
+    return next_map
+
+
+seeds = [
+    int(character) for character in read_lines.pop(0).split(" ") if character.isdigit()
+]
+steps = get_steps_maps_from_lines(read_lines)
+
+res = []
 for seed in seeds:
-    total_points += seed
+    next = seed
+    for key, values in steps.items():
+        next = get_map_for_next_step(values, next)
+    res.append(next)
+
+print(min(res))
