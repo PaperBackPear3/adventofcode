aaf6b505d766aa0d2e3a8f6c6c3c299d6ffd5323
PaperBackPear3 <fabioangeloni2001@gmail.com>
2023-12-08 10:59:49 +0100

    finished part2

--- 2023/day8/part2.py
+++ 2023/day8/part2.py
@@ -1,6 +1,7 @@
 import os
 import time
 import sys
+import math
 
 sys.setrecursionlimit(100000)
 file_name = sys.argv[1]
@@ -28,24 +29,29 @@ def prepare_input(read_lines):
 
 
 parsed_input = prepare_input(read_lines)
-
+starting_inputs = [key for key in parsed_input if key.endswith("A")]
+finishing_inputs = [key for key in parsed_input if key.endswith("Z")]
 global_count = 0
+GLOBAL_LETTER_LENGHT = len(letter_maps)
 
 
 def get_next_move(node, index, count):
     # check if its last index if so reset it to start
-    if index == len(letter_maps):
+    if index == GLOBAL_LETTER_LENGHT:
         index = 0
     count += 1
     if letter_maps[index] == "L":
-        if node[0] == "ZZZ":
+        if node[0] in finishing_inputs:
             return count
         return get_next_move(parsed_input[node[0]], index + 1, count)
     elif letter_maps[index] == "R":
-        if node[1] == "ZZZ":
+        if node[1] in finishing_inputs:
             return count
         return get_next_move(parsed_input[node[1]], index + 1, count)
 
 
-res = get_next_move(parsed_input["AAA"], 0, global_count)
-print(res)
+res = []
+for start in starting_inputs:
+    res.append(get_next_move(parsed_input[start], 0, global_count))
+
+print(math.lcm(*res))
