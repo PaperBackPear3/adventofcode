05107c94784eaeed116f71e1391ce76fe7d7a96d
Fabio Angeloni <fabioangeloni@fattureincloud.it>
2023-12-12 17:49:51 +0100

    readinput

--- 2023/day12/main.py
+++ 2023/day12/main.py
@@ -13,9 +13,21 @@ file1 = open(dir_path + "/" + file_name, "r")
 read_lines = file1.read().splitlines(keepends=False)
 file1.close()
 
+def find_possible_patters_in_spring_string(spring_string,line_quantities:list):
+  for quantity in line_quantities:
+    #first check for how many # closer (len + or - 1 to the quantity) to numers in line_quantities
+    #then checks for the number of ? in the string closer to the quantity (len + or - 1 to the quantity)
+    
+
 springs = []
-line_characters = []
+springs_quantities = []
 for line in read_lines:
+  split_line = line.split(" ")
+  springs.append(split_line[0])
+  springs_quantities.append([int(x) for x in split_line[1].split(",")])
+  
+print(springs)
+print(line_characters)
 
 
 # check for already existing patterns like
