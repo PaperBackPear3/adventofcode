4ceaad9be589285be8298683711a5656cec0a269
Fabio Angeloni <fabioangeloni@fattureincloud.it>
2024-12-14 14:14:36 +0100

    day 10 and 13

--- 2024/day10/main.go
+++ 2024/day10/main.go
@@ -10,31 +10,42 @@ import (
 // and always increases by a height of exactly 1 at each step.
 // Hiking trails never include diagonal steps
 // only up, down, left, or right (from the perspective of the map).
+
+type Cell struct {
+	value   int
+	visited bool
+}
+
 func main() {
 	lines := utils.ReadAndSplitRows("./test.txt", false, "")
 	var startPositions []models.Coordinates
-	linesToNumber := [][]int{}
 
-	directions := []models.Coordinates{
-		{X: 0, Y: -1}, // up
-		{X: 0, Y: 1},  // down
-		{X: -1, Y: 0}, // left
-		{X: 1, Y: 0},  // right
-	}
+	worldMap := [][]Cell{}
 
 	for y, line := range lines {
-		linesToNumber = append(linesToNumber, utils.ArrayAtoi(line))
+		row := []Cell{}
 		for x, char := range line {
-			if char == string('0') {
+			number := utils.Atoi(char)
+			row = append(row, Cell{value: number, visited: false})
+			if char == "0" {
 				startPositions = append(startPositions, models.Coordinates{X: x, Y: y})
 			}
 		}
+		worldMap = append(worldMap, row)
 	}
+	part1(worldMap, startPositions)
+}
 
+//a valid path is one that starts at 0 and arrives at 9
+//from all starting position floodFill until it finds a 9
+// to move from one cell to the next one there must be a difference of at most 1. 0 can go to 1 and 1 to 2 not more.
+
+func part1(worldMap [][]Cell, startPositions []models.Coordinates) {
+	paths := make(map[models.Coordinates]int)
 	for _, start := range startPositions {
-		// Process each starting position
-		// Example: print the starting position
-		println("Starting position:", start.X, start.Y)
 	}
 
+	for start, count := range paths {
+		println("Starting position:", start.X, "Total paths to 9:", count)
+	}
 }
--- 2024/day13/main.go
+++ 2024/day13/main.go
@@ -13,7 +13,7 @@ type Machine struct {
 
 func main() {
 
-	lines := utils.ReadFileLineByLine("./test.txt")
+	lines := utils.ReadFileLineByLine("./input.txt")
 	input := []Machine{}
 	newMachine := Machine{}
 	for lineIndex, line := range lines {
@@ -31,8 +31,8 @@ func main() {
 				newMachine.buttonB.Y = utils.Atoi(tmp[2])
 			}
 			if tmp[0] == "Prize" {
-				newMachine.prize.X = utils.Atoi(tmp[1])
-				newMachine.prize.Y = utils.Atoi(tmp[2])
+				newMachine.prize.X = utils.Atoi(tmp[1]) * 1000000000
+				newMachine.prize.Y = utils.Atoi(tmp[2]) * 1000000000
 			}
 		}
 		if line == "" || lineIndex == len(lines)-1 {
@@ -45,4 +45,76 @@ func main() {
 }
 
 func part1(machines []Machine) {
+
+	type ButtonPressed struct {
+		ButtonAPressedCount int
+		ButtonBPressedCount int
+	}
+	allWorkingCombos := []ButtonPressed{}
+
+	type AllMachinesList struct {
+		machineId        int
+		allWorkingCombos []ButtonPressed
+	}
+
+	allMachines := []AllMachinesList{}
+
+	for machineID, test := range machines {
+		allWorkingCombos = []ButtonPressed{}
+		prizeX := test.prize.X
+		prizeY := test.prize.Y
+
+		buttonAPress := 0
+		buttonBPress := 0
+
+		currentX := 0
+		currentY := 0
+
+		for {
+			for {
+				if currentX < prizeX && currentY < prizeY {
+					buttonBPress = buttonBPress + 1
+					currentX = currentX + test.buttonB.X
+					currentY = currentY + test.buttonB.Y
+				} else if currentX == prizeX && currentY == prizeY {
+					allWorkingCombos = append(allWorkingCombos, ButtonPressed{ButtonAPressedCount: buttonAPress, ButtonBPressedCount: buttonBPress})
+					buttonBPress = 0
+					currentX = buttonAPress * test.buttonA.X
+					currentY = buttonAPress * test.buttonA.Y
+					break
+				} else {
+					buttonBPress = 0
+					currentX = buttonAPress * test.buttonA.X
+					currentY = buttonAPress * test.buttonA.Y
+					break
+				}
+			}
+			if currentX < prizeX && currentY < prizeY {
+				buttonAPress = buttonAPress + 1
+				currentX = currentX + test.buttonA.X
+				currentY = currentY + test.buttonA.Y
+			} else if currentX == prizeX && currentY == prizeY {
+				allWorkingCombos = append(allWorkingCombos, ButtonPressed{ButtonAPressedCount: buttonAPress, ButtonBPressedCount: buttonBPress})
+				buttonAPress = 0
+				break
+			} else {
+				buttonAPress = 0
+				break
+			}
+		}
+		if len(allWorkingCombos) > 0 {
+			allMachines = append(allMachines, AllMachinesList{
+				machineId:        machineID,
+				allWorkingCombos: allWorkingCombos,
+			})
+		}
+	}
+	globalCost := 0
+	for _, machine := range allMachines {
+		for _, combo := range machine.allWorkingCombos {
+			cost := combo.ButtonAPressedCount*3 + combo.ButtonBPressedCount
+			globalCost = globalCost + cost
+		}
+	}
+	println("global cost: ", globalCost)
 }
